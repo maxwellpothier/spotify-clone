c551539539950508e3aeef29babb4791d87265d6
Max Pothier <max@smartystreets.com>
2022-08-08 13:00:52 -0600

    adding auth functionality to the frontend

---
+++ pages/signin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthForm from "../components/authForm";
+
+const Signin = () => {
+    return (
+        <AuthForm mode={"signin"}/>
+    );
+};
+
+Signin.authPage = true;
+
+export default Signin;
