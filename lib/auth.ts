561dd100af26e376ddfb763c3b7d21f74d211dd6
Max Pothier <max@smartystreets.com>
2022-08-11 12:58:33 -0600

    wires up middleware and auth to frontend

---
+++ lib/auth.ts
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import { MdFastRewind } from "react-icons/md";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+    return async (req: NextApiRequest, res: NextApiResponse) => {
+        const {TRAX_ACCESS_TOKEN: token} = req.cookies;
+
+        if (token) {
+            let user;
+            
+            try {
+                const {id} = jwt.verify(token, "Hello");
+
+                user = await prisma.user.findUnique({
+                    where: {id},
+                });
+
+                if (!user) {
+                    throw new Error("Not real user");
+                }
+            } catch (err) {
+                res.status(401);
+                res.json({error: "Not Authorized"});
+                return;
+            }
+            
+            return handler(req, res, user);
+        }
+
+        res.status(401);
+        res.json({error: "Not Authorized"});
+    };
+};
---
+++ pages/_middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+const signedInPages = [
+    "/",
+    "/playlist",
+    "/library",
+];
+
+export default function middleware(req) {
+    if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
+        const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+        if (!token) {
+            return NextResponse.redirect(`${req.nextUrl.origin}/signin`);
+        }
+    }
+};
